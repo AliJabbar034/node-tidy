f9300a0530adbdeb20cae77e435fee485240e7f1
alijabbar034 <alijabbar0034@gmail.com>
2025-01-14 22:27:13 +0500

    fixed regex issue

--- src/utils/InstallMissingPackages.js
+++ src/utils/InstallMissingPackages.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const glob = require("glob");
 const getDependencies = require("./getDependencies");
+
 function findMissingPackages() {
   console.log("Scanning for missing packages...");
 
@@ -10,14 +11,14 @@ function findMissingPackages() {
 
   const usedPackages = new Set();
   const importRegex =
-    /(?:import\s+(?:[\w*\s{},]+from\s+)?|import\s+['"]|require\(['"])([^'"]+)(?:['"]\))/g;
+    /\b(?:import(?:["'\s]*[\w*{}\n, ]+from\s*)?["']([^"']+)["']|require\(["']([^"']+)["']\))/g;
 
   for (const file of files) {
     const content = fs.readFileSync(file, "utf8");
     let match;
     while ((match = importRegex.exec(content)) !== null) {
-      const packageName = match[1].split("/")[0]; // Extract the package name
-      if (!packageName.startsWith(".")) {
+      const packageName = (match[1] || match[2] || "").split("/")[0]; // Extract the package name
+      if (packageName && !packageName.startsWith(".")) {
         usedPackages.add(packageName);
       }
     }
